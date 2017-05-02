725559f79ddc3c8cd9a8f24b3c2c12580b1470df
Jackson Saylor <jackson.saylor@yahoo.com>
2017-05-02 18:49:11 -0400

    Changed formatting a bit

--- SameSum/Program.cs
+++ SameSum/Program.cs
@@ -19,51 +19,49 @@ namespace SameSum
 
         static bool SomeMethod(int[] numbers, ref int count)
         {
-            if (count == numbers.Length)
+            while (count != numbers.Length)
             {
-                return false;
-            }
+                int[] TempArray1;
+                int[] TempArray2;
 
-            int[] TempArray1;
-            int[] TempArray2; 
-
-            for(int i = 1; i < numbers.Length; i++)
-            {
-                TempArray1 = new int[i];
-                TempArray2 = new int[numbers.Length - i];
-
-                TempArray1 = FillArray(numbers, TempArray1);
-                TempArray2 = FillArray(numbers, TempArray1);
-
-                if (TempArray1.Sum() == TempArray2.Sum())
+                for (int i = 1; i < numbers.Length; i++)
                 {
-                    return true;
-                }
-            }
+                    TempArray1 = new int[i];
+                    TempArray2 = new int[numbers.Length - i];
 
-            #region Make New Array
-            int[] tempArray = numbers;
-            for (int i = 0; i <= numbers.Length; i++)
-            {
-                if (i == 0)
-                {
-                    numbers[i] = tempArray[numbers.Length - 1];
-                }
-                else
-                {
-                    numbers[i] = tempArray[i - 1];
-                }
-            }
-            #endregion
+                    TempArray1 = FillArray(numbers, TempArray1);
+                    TempArray2 = FillArray(numbers, TempArray1);
 
-            count++;
-            SomeMethod(numbers, ref count);
-            return false; 
+                    if (TempArray1.Sum() == TempArray2.Sum())
+                    {
+                        return true;
+                    }
+                }
+
+                #region Make New Array
+                int[] tempArray = numbers;
+                for (int i = 0; i <= numbers.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        numbers[i] = tempArray[numbers.Length - 1];
+                    }
+                    else
+                    {
+                        numbers[i] = tempArray[i - 1];
+                    }
+                }
+                #endregion
+
+                count++;
+                SomeMethod(numbers, ref count);
+            }
+            return false;
         }
 
         static int[] FillArray(int[] filler, int[] filled)
         {
-            for(int i = 0; i < filled.Length; i++)
+            for (int i = 0; i < filled.Length; i++)
             {
                 filled[i] = filler[i]; 
             }
